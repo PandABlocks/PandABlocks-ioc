f9d21fcb2623b3770f22ba513ac1a98c62c4980e
AlexanderWells-diamond <AlexanderWells-diamond@users.noreply.github.com>
2023-04-13 10:22:34 +0000

    deploy: 3deab0ef36231e6f09bc71d2018b41bf38c0f6be

--- remove-file-server/_static/documentation_options.js
+++ remove-file-server/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev263+g116608c',
+    VERSION: '0.1.dev263+g3deab0e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
