29cfff0d79c33cc63a433af6afab43024f872cdd
evalott100 <evalott100@users.noreply.github.com>
2023-11-22 11:32:49 +0000

    deploy: b778231007cce686580c637e504f73c7e36a487a

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.2.2b4.dev21+g09b42fa',
+    VERSION: '0.2.2b4.dev22+gb778231',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
