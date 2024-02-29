03ca1b8fe2f3d6301ee660b071a73b878682e100
Eva Lott <evalott100@gmail.com>
2024-02-29 10:18:18 +0000

    cleaned cleaned up merge

--- src/pandablocks_ioc/__init__.py
+++ src/pandablocks_ioc/__init__.py
@@ -1,16 +1,8 @@
-<<<<<<< before updating
-import sys
+from importlib.metadata import version  # noqa
+
 from .ioc import create_softioc
 
-if sys.version_info < (3, 8):
-    from importlib_metadata import version  # noqa
-else:
-    from importlib.metadata import version  # noqa
-
-__version__ = version("pandablocks-ioc")
+__version__ = version("PandABlocks-ioc")
 del version
-=======
-from ._version import __version__
->>>>>>> after updating
 
 __all__ = ["__version__", "create_softioc"]
