58926f7a3e3c9e0c92ad7b6298a388d405a5ae3d
jwlodek <jwlodek@users.noreply.github.com>
2026-08-14 19:34:39 +0000

    deploy: 35a50a998313b6207452b0dcd30595870c60e179

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.11.6.dev8+gc9ee0f884',
+    VERSION: '0.11.6.dev11+g35a50a998',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
